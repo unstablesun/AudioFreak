bc0668bc3f93afd3134ed15a87d518e774622728
unstablesun <unstablesun@gmail.com>
2016-02-25 15:20:27 -0800

    init commit

---
+++ Assets/README.txt
@@ -0,0 +1,11 @@
+		41 Post
+Created by DimasTheDriver on May/17/2012 . 
+Part of the 'Unity: Making a simple audio visualization' post. http://www.41post.com/?p=4776
+
+Instructions:
+
+Everything has been placed inside the Main scene, that can be found at the 'Scenes' folder. The AudioVisualizer script is attached to the Audio Visualization game object.
+
+
+Music Credits:
+	"Kick Shock" by Kevin MacLeod (incompetech.com) Licensed under Creative Commons "Attribution 3.0" http://creativecommons.org/licenses/by/3.0/"
---
+++ Assets/Scripts/Other/BackToPost.cs
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////
+//                             41 Post                  //          
+// Created by DimasTheDriver on May/17/2012             //                         
+// Part of 'Unity: Making a simple audio visualization' //           		 		
+// Available at:     http://www.41post.com/?p=4776      //                          
+//////////////////////////////////////////////////////////
+
+using UnityEngine;
+using System.Collections;
+
+public class BackToPost : MonoBehaviour 
+{
+	void OnGUI()
+	{
+		//if(GUI.Button(new Rect(Screen.width-110,Screen.height-40,100,30),"Back to post..."))
+		//{
+		//	Application.OpenURL("http://www.41post.com/?p=4776");
+		//	
+		//}
+	}
+}
